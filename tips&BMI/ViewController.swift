00760166b167cc4d99d0c1dccc15349f27a21ef6
YeeGuo <kiwi881217@gmail.com>
2023-04-06 17:00:43 +0800

    Initial Commit

---
+++ tips&BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tips&BMI
+//
+//  Created by 張凱棋 on 2023/4/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
